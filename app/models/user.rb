7b7e9d117a9659e7b5d210950a2a1e8f16a4e5f1
William Galarza <w.galarza234@gmail.com>
2015-04-01 21:57:41 -0400

    Completed project

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :name
+  has_many :projects
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
